630a8029bb444a5edbcb8852cb641ac3062c790a
Yakanna <167448285+yakanna2000@users.noreply.github.com>
2025-04-09 14:28:57 +0530

    Update amstrong.java

--- amstrong.java
+++ amstrong.java
@@ -1,26 +1,10 @@
 import java.util.*;
 
-import javax.crypto.interfaces.DHKey;
+
 public class amstrong {
     public static void main(String[] args)
     {
-       int n,c,r,sum=0;
-       Scanner am=new Scanner(System.in);
-       System.out.println("enter the num");
-       n=am.nextInt();
-       c=n;
-       while(n>0)
-       {
-          r=n%10;
-          sum=sum+(r*r*r);
-          n=n/10;
-       }
-       if(c==sum)
-       {
-        System.out.println("amstrong");
-       }
-       else{
-        System.out.println("not amstrong");
-       }
+      System.out.println("hi");
+
     }
 }
